8482c8daedbb1f4049d7e74d61751a80b50ba098
FinlayShepherd <fa296@kent.ac.uk>
2018-08-09 14:47:42 +0100

    Skeleton classes

---
+++ src/risk.py
@@ -0,0 +1,11 @@
+class Risk:
+
+    def __init__(self):
+        self.board = self.create_board()
+
+    def create_board(self):
+        return 1
+
+    def get_board(self):
+        return self.board
+
